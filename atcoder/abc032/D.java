3de38f302e50a9fac01931b976dbc425ee913048
agata <ymgtech@millenary.net>
2019-12-16 22:50:52 +0900

    [add] ABC032_D 4回目（メモ化高速）

--- atcoder/abc032/D.java
+++ atcoder/abc032/D.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 import java.util.Map;
+import java.util.Optional;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -20,41 +21,36 @@ public class D {
         W = sc.nextInt();
         v = new long[N];
         w = new long[N];
-        calculated = new HashMap<Integer, Map<Long, Long>>();
-        for(int i = 0; i < N; i++) {
-            calculated.put(i, new HashMap<Long, Long>());
-        }
+        calculated = new HashMap<>();
         for(int i = 0; i < N; i++) {
             v[i] = sc.nextLong();
             w[i] = sc.nextLong();
+        }
+        for(int i = 0; i < N + 1; i++) {
+            calculated.put(i, new HashMap<Long, Long>());
         }
 
         System.out.println(rec(0, W));
     }
 
-    public static long rec(int i, long j) {
-        long res;
+    public static Long rec(int i, long j) {
+        Long res = calculated.get(i).get(j);
+        if(res != null) {
+            return res;
+        }
+
         /* 品物は 0〜N-1 個目まであり、即ち N 個以上は「使える品が無い」として 0 である */
         if(i == N) {
-            return 0;
+            res = 0L;
         /* 品物が容量不足で入れられないとき、入れないまま次の品物を入れる */
         } else if(j < w[i]) {
-            if(calculated.get(i).get(j) != null) {
-                return calculated.get(i).get(j);
-            } else {
-                res = rec(i + 1, j);
-                calculated.get(i).put(j, res);
-                return res;
-            }
+            res = rec(i + 1, j);
         } else {
-            if(calculated.get(i).get(j) != null) {
-                return calculated.get(i).get(j);
-            } else {
-                res = Math.max(v[i] + rec(i + 1, j - w[i]), rec(i + 1, j));
-                calculated.get(i).put(j, res);
-                return res;
-            }
+            res = Math.max(v[i] + rec(i + 1, j - w[i]), rec(i + 1, j));
         }
+
+        calculated.get(i).put(j, res);
+        return res;
     }
 
 }
