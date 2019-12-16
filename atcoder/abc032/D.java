2cdc093be58f5b60762c6d087c9be898bbb2f2fb
agata <ymgtech@millenary.net>
2019-12-16 22:22:10 +0900

    [add] ABC32_D 2回目（メモ化）

--- atcoder/abc032/D.java
+++ atcoder/abc032/D.java
@@ -1,21 +1,29 @@
+import java.util.Arrays;
+import java.util.Map;
+import java.util.HashMap;
 import java.util.Scanner;
 
 /**
  * https://abc032.contest.atcoder.jp/tasks/abc032_d
  */
-public class Main {
+public class D {
 
     static long[] v;
     static long[] w;
     static int N;
-    static long W;
+    static int W;
+    static Map<Integer, Map<Long, Long>> calculated;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         N = sc.nextInt();
-        W = sc.nextLong();
+        W = sc.nextInt();
         v = new long[N];
         w = new long[N];
+        calculated = new HashMap<Integer, Map<Long, Long>>();
+        for(int i = 0; i < N; i++) {
+            calculated.put(i, new HashMap<Long, Long>());
+        }
         for(int i = 0; i < N; i++) {
             v[i] = sc.nextLong();
             w[i] = sc.nextLong();
@@ -30,9 +38,20 @@ public class Main {
             return 0;
         /* 品物が容量不足で入れられないとき、入れないまま次の品物を入れる */
         } else if(j < w[i]) {
-            return rec(i + 1, j);
+            if(calculated.get(i).get(j) != null) {
+                return calculated.get(i).get(j);
+            } else {
+                calculated.get(i).put(j, rec(i + 1, j));
+                return calculated.get(i).get(j);
+            }
         } else {
-            return Math.max(v[i] + rec(i + 1, j - w[i]), rec(i + 1, j));
+            if(calculated.get(i).get(j) != null) {
+                return calculated.get(i).get(j);
+            } else {
+                calculated.get(i).put(j, Math.max(v[i] + rec(i + 1, j - w[i]), rec(i + 1, j)));
+                return calculated.get(i).get(j);
+            }
         }
     }
+
 }
