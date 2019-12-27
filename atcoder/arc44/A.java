52a9399e79257f7028dacc82ab8c7df31e86beb9
agata <ymgtech@millenary.net>
2019-12-28 00:38:25 +0900

    [add] ARC044_A 1回目 実装

---
+++ atcoder/arc44/A.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class A {
+
+	public static void main(String[] args) {
+		int n = new Scanner(System.in).nextInt();
+
+		if(n == 1) {
+			System.out.println("Not Prime");
+		} else if(n == 2) {
+			System.out.println("Prime");
+		} else if(n % 2 == 0) {
+			System.out.println("Not Prime");
+		} else {
+			for(int i = 3; i <= Math.sqrt(n); i += 2) {
+				if(n % i == 0) {
+					System.out.println((sumDigits(n) % 3 == 0 || n % 10 == 5) ? "Not Prime" : "Prime");
+					return;
+				}
+			}
+			System.out.println("Prime");
+		}
+	}
+
+	public static int sumDigits(int n) {
+		int sum = 0;
+
+		while(n != 0) {
+			sum += n % 10;
+			n /= 10;
+		}
+
+		return sum;
+	}
+
+}
