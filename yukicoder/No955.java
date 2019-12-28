1ff473dc69d9f9dd2a4e3b3f13bf54a6ea01c46c
agata <ymgtech@millenary.net>
2019-12-28 11:08:07 +0900

    [add] yukicoder No.955 4,5回目

--- yukicoder/No955.java
+++ yukicoder/No955.java
@@ -1,3 +1,5 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Scanner;
 
 public class No955 {
@@ -25,17 +27,32 @@ public class No955 {
 			}
 		}
 
-		long D = b * b - 4 * a * c;
-		if(D > 0) {
+		BigDecimal A = new BigDecimal(a);
+		BigDecimal B = new BigDecimal(b);
+		BigDecimal C = new BigDecimal(c);
+
+		BigDecimal D = new BigDecimal(b);
+		D = D.multiply(B).subtract(new BigDecimal(4).multiply(A).multiply(C));
+
+		if(D.compareTo(BigDecimal.ZERO) == 1) {
 			System.out.println(2);
-			System.out.println((-b - Math.pow(D, 0.5f)) / 2 / a);
-			System.out.println((-b + Math.pow(D, 0.5f)) / 2 / a);
-		} else if(D == 0) {
+			System.out.println(BigDecimal.ZERO.subtract(B).subtract(sqrt(D)).divide(new BigDecimal(2).multiply(A), 20, RoundingMode.HALF_EVEN));
+			System.out.println(BigDecimal.ZERO.subtract(B).add(sqrt(D)).divide(new BigDecimal(2).multiply(A), 20, RoundingMode.HALF_EVEN));
+		} else if(D.compareTo(BigDecimal.ZERO) == 0) {
 			System.out.println(1);
-			System.out.println((int)(-b + Math.pow(D, 0.5f)) / 2 / a);
+			System.out.println(BigDecimal.ZERO.subtract(B).divide(new BigDecimal(2).multiply(A), 20, RoundingMode.HALF_EVEN));
 		} else {
 			System.out.println(0);
 		}
+	}
+
+	public static BigDecimal sqrt(BigDecimal a) {
+		BigDecimal a2 = a.add(BigDecimal.ZERO);
+
+		for(int i = 0; i < 50; i++) {
+			a2 = a2.subtract(a2.pow(2).subtract(a).divide(a2.multiply(new BigDecimal(2)), 20, RoundingMode.HALF_EVEN));
+		}
+		return a2;
 	}
 
 }
