1e6f3ee09fad8fb64638b145ae97620706fccfef
agata <ymgtech@millenary.net>
2019-12-28 09:31:57 +0900

    [add] yukicoder No.955 2回目

--- yukicoder/No955.java
+++ yukicoder/No955.java
@@ -8,6 +8,17 @@ public class No955 {
 		int a = sc.nextInt();
 		int b = sc.nextInt();
 		int c = sc.nextInt();
+		
+		if(a == 0) {
+			if(b == 0) {
+				System.out.println(-1);
+				return;
+			} else {
+				System.out.println(1);
+				System.out.println(-c);
+				return;
+			}
+		}
 
 		long D = b * b - 4 * a * c;
 		if(D > 0) {
